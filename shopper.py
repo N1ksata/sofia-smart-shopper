7f995d488e074038be5c1bb6811e44a97d779579
Niksata <niki.kolev2006@gmail.com>
2026-06-19 17:13:38 +0300

    import json

--- shopper.py
+++ shopper.py
@@ -1,1 +1,11 @@
 import json
+
+def load_prices():
+    with open('market_prices.json', 'r') as f:
+        data = json.load(f)
+        return data
+
+if __name__ == "__main__":
+    grocery_data = load_prices()
+    print("Successfully loaded data:")
+    print(grocery_data)
